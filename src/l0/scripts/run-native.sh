e828a5f72018435b1ad18c4990eea905ca9fbf55
Abhishek Borar <abhishekborar93@gmail.com>
2025-11-17 21:13:13 +0530

    kiwi-clean-3

--- src/l0/scripts/run-native.sh
+++ src/l0/scripts/run-native.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# install packages only if missing
+pkgs=(container-selinux passt-selinux) # these are needed for kiwi to build images containing package:patterns-containers-container_runtime
+missing=()
+for p in "${pkgs[@]}"; do
+  if ! rpm -q "$p" >/dev/null 2>&1; then
+    missing+=("$p")
+  fi
+done
+
+if [ "${#missing[@]}" -ne 0 ]; then
+  echo "Installing missing packages: ${missing[*]}"
+  sudo zypper -n in "${missing[@]}"
+else
+  echo "All required packages already installed: ${pkgs[*]}"
+fi
+
 VERSION=0.1.0-dev
 # Use first argument as the profile, default to "Standard"
 PROFILE="${1:-Standard}"
